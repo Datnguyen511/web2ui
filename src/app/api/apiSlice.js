d19f4243a1072346f0510ce34ff81248caf909b6
Dat <dat.nguyen@hcmut.edu.vn>
2023-09-14 00:06:52 +0700

    Uploaded

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://web2technote-api.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://web2technote-api.onrender.com' }),
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
