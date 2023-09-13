9fe393f90080e55744f5666874a65091e4573451
Datnguyen511 <128125543+Datnguyen511@users.noreply.github.com>
2023-09-14 00:56:00 +0700

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'https://web2technote-api.onrender.com' }),
+    credentials: 'include',
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
